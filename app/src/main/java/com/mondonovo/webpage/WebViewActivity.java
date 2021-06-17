f75823f4a64ceb4c493715b5c7d154ebe671c6cb
Mirko <cmirko@gmail.com>
2021-06-17 16:36:42 +0200

    check connection commit

--- app/src/main/java/com/mondonovo/webpage/WebViewActivity.java
+++ app/src/main/java/com/mondonovo/webpage/WebViewActivity.java
@@ -6,13 +6,21 @@ referring that site
 
 package com.mondonovo.webpage;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Objects;
 
@@ -23,6 +31,12 @@ public class WebViewActivity extends AppCompatActivity {
     This is true whether they are opened by JavaScript or by the target attribute in a link
      */
     WebView siteWebView;
+
+    /*
+    ENTER HERE THE URL. It should be an https site preferably, this is not the case
+    so in the manifest android:usesCleartextTraffic="true" is needed
+    */
+    static String URL = "http://www.campingdewijnstok.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +52,43 @@ public class WebViewActivity extends AppCompatActivity {
         WebSettings webSettings = siteWebView.getSettings();
         webSettings.setJavaScriptEnabled(false);
 
-        //ENTER HERE THE URL to be opened. It should be an https site preferably, this is not one of
-        //them so in the manifest android:usesCleartextTraffic="true" is needed
-        siteWebView.loadUrl("http://www.campingdewijnstok.com");
+        SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipe_to_update);
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            if (isConnected()) {
+                webViewHelper();
+                swipeRefreshLayout.setRefreshing(false);
+            } else {
+                swipeRefreshLayout.setRefreshing(false);
+                Snackbar.make(siteWebView, R.string.no_connection, Snackbar.LENGTH_SHORT).show();
+            }
+        });
 
-        //When the user clicks a link from a web page in your WebView,
-        // the default behavior is for Android to launch an app that handles URLs.
-        // Usually, the default web browser opens and loads the destination URL.
-        // However, you can override this behavior for your WebView, so links open
-        // within your WebView. You can then allow the user to navigate backward and
-        // forward through their web page history that's maintained by your WebView
+        //check if internet connection is already active (either wifi or mobile)
+        if (isConnected()) {
+            webViewHelper();
+        } else {
+            AlertDialog checkConnectionDialog = new AlertDialog.Builder(this)
+                    .setTitle(R.string.no_connection)
+                    .setMessage(R.string.swipe_to_update)
+                    // close this app
+                    .setNegativeButton(R.string.ok, (dialog, which) -> dialog.dismiss())
+                    .create();
+            checkConnectionDialog.show();
+        }
+    }
+
+    private void webViewHelper() {
+
+        siteWebView.loadUrl(URL);
+
+        /*
+        When the user clicks a link from a web page in your WebView,
+        the default behavior is for Android to launch an app that handles URLs.
+        Usually, the default web browser opens and loads the destination URL.
+        However, you can override this behavior for your WebView, so links open
+         within your WebView. You can then allow the user to navigate backward and
+        forward through their web page history that's maintained by your WebView
+        */
         siteWebView.setWebViewClient(new CustomViewClient());
     }
 
@@ -73,8 +114,23 @@ public class WebViewActivity extends AppCompatActivity {
             return true;
         }
 
-        // If it wasn't the Back key or there's no web page history, bubble up to the default
-        // system behavior (probably exit the activity)
+        /*
+        If it wasn't the Back key or there's no web page history, bubble up to the default
+        system behavior (exit the activity)
+        */
         return super.onKeyDown(keyCode, event);
+    }
+
+    private boolean isConnected() {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        /*
+        returns a NetworkInfo instance representing the first connected network interface
+        it can find, or null if none of the interfaces is connected (meaning that an
+        internet connection is not available)
+        */
+        return networkInfo != null && networkInfo.isConnected();
     }
 }
